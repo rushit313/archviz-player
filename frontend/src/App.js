0cc039035a4264555f242c53eff2eaf828f65309
rushit313 <rushit.dani@gmail.com>
2025-07-16 13:10:31 +0530

    Initial working version

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoPlayer from "./components/VideoPlayer";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", margin: "20px 0" }}>ArchViz Player</h1>
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
